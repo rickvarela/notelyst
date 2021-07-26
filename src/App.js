f674ab03898952e916514563af00d07fbd070f53
Rick Varela <rickvarela91@gmail.com>
2021-07-25 20:13:10 -0700

    Implement gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 import { NoteLystApp } from './sections/NoteLystApp';
